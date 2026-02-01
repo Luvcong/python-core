00f78129e0935302d1f57e7b5d756fbb46ead4f8
jinheekim <142660595+Luvcong@users.noreply.github.com>
2026-02-02 08:17:25 +0900

    section_05 : Python Advanced(4)

---
+++ chapter05_02.py
@@ -0,0 +1,35 @@
+"""
+chapter05_02
+Python Advanced(4) - 나만의 패키지
+Keyword - png(jpg) to gif, pil, image
+"""
+ 
+"""
+- 패키지 작성 : 정적 이미지(JPG, PNG) -> GIF(애니메이션)이미지 변환 패키지
+"""
+
+import glob
+from PIL import Image
+
+# 이미지, 결과 생성 경로
+path_in = "./project/images/*.png"
+path_out = "./project/image_out/result.gif"
+
+# 첫번째 이미지 & 모든 이미지 리스트 packing
+img, *images = [Image.open(f) for f in sorted(glob.glob(path_in))]
+
+# 라사이즈(필요한 경우)
+img, *images = [Image.open(f).resize((300, 400), Image.Resampling.LANCZOS) for f in sorted(glob.glob(path_in))]
+
+# 이미지 저장
+img.save(
+    fp=path_out, 
+    format = 'GIF', 
+    append_images = images,
+    save_all = True, 
+    duration = 100, 
+    loop = 0,
+    disposal = 2
+)
+print(img)
+print(images)
